5dbc1eb117c29fe0816647309785201c837db4b8
tosxin-456 <tosinekshally@gmail.com>
2024-05-02 08:24:15 +0100

    worked on the selection and ranking

--- spearmanRank.js
+++ spearmanRank.js
@@ -1,2 +1,28 @@
-const X = []
-const Y = []
+const X = [4, 6, 3, 8, 2, 0];
+const Y = [2, 3, 4, 5, 6, 7];
+
+function rankingOfNumbers(array) {
+    const sortedArray = [...array];
+
+    sortedArray.sort((a, b) => a - b);
+
+    const arrayRank = {};
+
+    sortedArray.forEach(num => {
+        arrayRank[num] = 0;
+    });
+
+    // Assign ranks
+    sortedArray.forEach((num, index) => {
+        arrayRank[num] += index + 1;
+    });
+
+    // Map original numbers to their ranks
+    const rankednumbers = array.map(number => arrayRank[number]);
+
+    return rankednumbers;
+}
+
+const rankedX = rankingOfNumbers(X);
+const rankedY = rankingOfNumbers(Y)
+console.log([rankedX, rankedY]);
