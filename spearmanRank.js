c4a831005e95fa77e271963b3e38dc643abc007f
tosxin-456 <tosinekshally@gmail.com>
2024-05-09 12:25:53 +0100

    made it workable with node.js

--- spearmanRank.js
+++ spearmanRank.js
@@ -1,60 +1,70 @@
-const X = [5,3,5,4];
-const Y = [5,4,4,5];
+const { stdout } = require('process');
+const readline = require('readline');
 
-function rankingOfNumbers(array) {
-    const sortedArray = [...array];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    sortedArray.sort((a, b) => a - b);
+let X, Y; 
 
-    const arrayRank = {};
-
-    sortedArray.forEach(num => {
-        arrayRank[num] = 0;
+rl.question("Enter values for x and separate with either a comma or space: ", function(inputX){
+    X = inputX.split(/[,\s]+/).map(item => parseInt(item.trim()));
+    
+    rl.question("Enter values for y and separate with either a comma or space: ", function(inputY){
+        Y = inputY.split(/[,\s]+/).map(item => parseInt(item.trim()));
+        if (X.length === Y.length) {
+            calculateSpearmanRank(X, Y);
+        } else {
+            console.log('The input values are not of equal length');
+        }
+        
+        rl.close();
     });
+});
 
-    // Assign ranks
-    sortedArray.forEach((num, index) => {
-        arrayRank[num] += index;
-    });
-
-    // Map original numbers to their ranks
-    const rankednumbers = array.map(number => arrayRank[number]);
-
-    return rankednumbers;
+function calculateSpearmanRank(X, Y) {
+    function rankingOfNumbers(array) {
+        const sortedArray = [...array];
+        sortedArray.sort((a, b) => a - b);
+        const arrayRank = {};
+        sortedArray.forEach(num => {
+            arrayRank[num] = 0;
+        });
+        sortedArray.forEach((num, index) => {
+            arrayRank[num] += index;
+        });
+        const rankedNumbers = array.map(number => arrayRank[number]);
+        return rankedNumbers;
+    }
+    
+    const rankedX = rankingOfNumbers(X);
+    const rankedY = rankingOfNumbers(Y);
+    
+    const d = rankedX.map((num, index) => num - rankedY[index]);
+    const dSquare = d.map(num => num ** 2);
+    const summationDSquare = dSquare.reduce((acc, num) => acc + num , 0);
+    
+    function calculatingTheSpearman_sRank(summationDSquare, X) {
+        const sixSummationDSquare = 6 * summationDSquare;
+        const n =  X.length;
+        const nSquareMInnusOne = n * (n ** 2 - 1);
+        const rank = 1 - sixSummationDSquare / nSquareMInnusOne;
+        return rank;
+    }
+    
+    const rank = calculatingTheSpearman_sRank(summationDSquare, X);
+    const spearRank  = 1 + rank;
+    const spearManRank = spearRank.toFixed(1);
+    console.log(rank);
+    
+    if (spearManRank >= 0.9) {
+        console.log('A very strong relationship, get married');
+    } else if (spearManRank >= 0.6 && spearManRank <= 0.8) {
+        console.log('Work on yourselves, or break up');
+    } else if (spearManRank >= 0.3 && spearManRank <= 0.5) {
+        console.log('Shaky ground, work on yourselves');
+    } else {
+        console.log('Divorce fast');
+    }
 }
-
-const rankedX = rankingOfNumbers(X);
-const rankedY = rankingOfNumbers(Y)
-const d = rankedX.map((num,index) =>num - rankedY[index]) 
-const dSquare = d.map(num=>num**2)
-const summationDSquare = dSquare.reduce((acc,num) => acc + num ,0)
-
-
-function calculatingTheSpearman_sRank (summationDSquare, X){
-    const sixSummationDSquare = 6 * summationDSquare
-    const n =  X.length
-    const nSquareMInnusOne = n*(n**2 - 1)
-    const rank = 1 - sixSummationDSquare/nSquareMInnusOne
-    return rank 
-}
-const rank = calculatingTheSpearman_sRank(summationDSquare, X);
-const spearRank  = 1 + rank
-const spearManRank = spearRank.toFixed(1)
-
-
-console.log(rank)
-
-if(spearManRank>=0.9){
-console.log('A very strong relatiionship, get married');
-}
-else if(spearManRank>=0.6 && spearManRank<=0.8){
-console.log('work on yourselves, or break up');
-}
-else if(spearManRank>=0.3 && spearManRank<=0.5){
-console.log('shaky ground , work on yourselfs');
-}
-else{
-console.log('divorce fast');
-
-}
-
