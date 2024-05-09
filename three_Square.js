0dcb7977ed01879ccd9d0f487797e69881fe3073
tosxin-456 <tosinekshally@gmail.com>
2024-05-09 15:31:20 +0100

    finished the three_square

--- three_Square.js
+++ three_Square.js
@@ -14,7 +14,7 @@ rl.question("Enter values for x and separate with either a comma or space: ", fu
         Y = inputY.split(/[,\s]+/).map(item => parseInt(item.trim()));
 
         if (X.length === Y.length) {
-            // calculateSpearmanRank(X, Y);
+            threeSquareValues(X, Y);
         } else {
             console.log('The input values are not of equal length');
         }
@@ -23,6 +23,24 @@ rl.question("Enter values for x and separate with either a comma or space: ", fu
     });
 });
 
- function threeSquare(){
+ function threeSquareValues(X,Y){
+  const n = X.length
+  let summationOfX = 0
+  let summationOfY = 0
+  let summationOfSquareOfX = 0
+  let summationOfXAndYMultiplicant = 0
+  for (let i = 0; i < X.length; i++ ){
+     summationOfX += X[i]
+    summationOfY += Y[i]
+    summationOfSquareOfX += X[i] ** 2
+    summationOfXAndYMultiplicant += X[i] * Y[i]
+  }
+  const squareOfSummationX = summationOfX ** 2
+  const numerator = (n * summationOfXAndYMultiplicant) - (summationOfX * summationOfY)
+  const denominator = (n * summationOfSquareOfX) - squareOfSummationX;
+  const m =  numerator / denominator
 
+  const b = (summationOfY - m * summationOfX)/ n
+ const equationOfLine = `${m}x + ${b}`
+  console.log(equationOfLine)
 }
